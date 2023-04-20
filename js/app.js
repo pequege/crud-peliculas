79bbf3ba577f6e4d2319677d6a043326b0820d16
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-04-20 17:25:20 -0300

    agrego persistencia del tema elegido con LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//leer de localStorage o usar valor por defecto
+let userTheme = JSON.parse(localStorage.getItem('theme')) || 'light';
+
+cambiarTema(userTheme);
+
 let btnThemeLight = document.getElementById('btnThemeLight')
 let btnThemeDark = document.getElementById('btnThemeDark')
 
@@ -6,5 +11,6 @@ btnThemeDark.addEventListener('click', ()=> cambiarTema('dark'));
 
 function cambiarTema(color){
     document.querySelector('html').setAttribute('data-bs-theme', color);
+    localStorage.setItem('theme', JSON.stringify(color));
     console.log(color)
 }
