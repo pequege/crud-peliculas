ed5d03cdc672eb9ef9a8874778940f6bb6fc903c
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-05-04 21:33:26 -0300

    agrego logica de leer peliculas desde localStorage

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let listaPeliculas = JSON.parse(localStorage.getItem("listaPeliculas")) || [];
+
+listaPeliculas.map((pelicula) => {
+    crearColumna(pelicula);
+})
+
+function crearColumna(pelicula){
+    let grilla = document.getElementById('grilla');
+    grilla.innerHTML += `
+    <aside class="col-lg-3 col-md-6 col-12 mt-3">
+        <div class="card h-100">
+            <img src="${pelicula.imagen}" class="card-img-top w-100" alt="poster de ${pelicula.titulo}">
+            <div class="card-body">
+                <h5 class="card-title d-inline-block">${pelicula.titulo}</h5>
+                <p class="card-text text-truncate">
+                ${pelicula.descripcion}
+                </p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary"><a href="pages/detalle.html" class="link-light text-decoration-none">Ver Detalle</a></button>
+            </div>
+        </div>
+    </aside>
+    `
+}
